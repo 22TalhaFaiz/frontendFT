eb5cbbb11d1822d21df0d6a6de49be0f69b5112e
22TalhaFaiz <138751622+22TalhaFaiz@users.noreply.github.com>
2025-09-22 23:24:09 +0500

    c

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -2,16 +2,16 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
-const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://backendft-production-9ad8.up.railway.app'
-  : 'http://localhost:3000'; // matches your index.js PORT & HOST
+// const API_URL = process.env.NODE_ENV === 'production'
+//   ? 'https://backendft-production-9ad8.up.railway.app'
+//   : 'http://localhost:3000'; // matches your index.js PORT & HOST
 
 const ProtectedRoute = ({ children }) => {
   const [authorized, setAuthorized] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(`${API_URL}/api/auth/dashboard`, {
+    axios.get(`https://backendft-production-9ad8.up.railway.app/api/auth/dashboard`, {
       withCredentials: true, // ✅ important to send session cookie
     })
     .then(() => {
